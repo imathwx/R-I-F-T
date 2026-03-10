049f9c5c73157aa57ff7fbd58a70ce22451613b1
imathwx <imathwx@gmail.com>
2026-03-10 16:52:22 -0300

    Update HWID.lua

--- HWID.lua
+++ HWID.lua
@@ -1,6 +1,6 @@
 local hwids = {
 	["fd34b1e163d47b355b08edb414565a7a7f473b4ba0d2dd12608e5fde3f8b2319e4c28bbd6d09bc4c1491b239803edd90e392ae6b0fa0e79ccc8b830b2ae07b38"] = "gorelio",
-  [""]
+	[""] = "lex"
 }
 
 return hwids
